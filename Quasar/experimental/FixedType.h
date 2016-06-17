a9dee126c25fc8f471762a821476fa0f8e1744b4
synkarae <synkarae@gmail.com>
2016-06-16 22:25:22 -0500

    Created experimental folder with FixedType.

---
+++ Quasar/experimental/FixedType.h
@@ -0,0 +1,46 @@
+/*
+ *   Copyright 2016 Robert C. Taylor
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+namespace Quasar
+{
+
+template <typename StoreType>
+class FixedType
+{
+public:
+	FixedType()
+	{
+		m_value = 0;
+		m_fractionalBits = sizeof(StoreType)*4;
+	}
+
+	FixedType(StoreType value)
+	{
+		m_value = value << m_fractionalBits;
+	}
+
+	FixedType(double value)
+	{
+		m_value = static_cast<StoreType>(value * std::pow(2, m_fractionalBits));
+	}
+
+
+private:
+	StoreType m_value;
+	std::size_t m_fractionalBits;
+};
+
+}
